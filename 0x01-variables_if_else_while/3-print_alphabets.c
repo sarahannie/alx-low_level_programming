5d1c3e77a63a2800ff03374ede0366277f86f761
sarahannie <87903267+sarahannie@users.noreply.github.com>
2022-10-16 09:56:33 -0400

    n

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,15 +4,15 @@
 
 /**
 * main - a-z lower case
-* Return: Always 0 (success) 
+* Return: Always 0 (success)
 */
 
 int main(void)
 {
 char n;
-for(n='a'; n <= 'z'; n++);
+for (n = 'a'; n <= 'z'; n++)
 putchar(n);
-for(n='A'; n <= 'Z'; n++);
+for(n = 'A'; n <= 'Z'; n++)
 putchar(n);
 putchar('\n');
 return (0);
