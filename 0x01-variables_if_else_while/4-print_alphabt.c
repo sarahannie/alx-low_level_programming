58fa38a88fdbdb8c7d79bf08d26e5c6b8d67b096
sarahannie <87903267+sarahannie@users.noreply.github.com>
2022-10-16 10:02:37 -0400

    n

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,17 @@
 
 /**
 * main - a-z lower case
-* Return: Always 0 (success) 
+* Return: Always 0 (success)
 */
-
 int main(void)
 {
-    char ch;
-    for(ch = 'a'; ch <= 'z'; ch++){
-    if(ch !='e' && ch !='q')
-    putchar(ch);
-    }
-    putchar('\n');
-    return (0);
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+
+if(ch != 'e' && ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
