9d85cc8c27c125a38b50fdfe841435a2ed97b027
sarahannie <87903267+sarahannie@users.noreply.github.com>
2022-10-16 10:16:47 -0400

    n

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,16 +4,16 @@
 
 /**
 * main - all combination of num witg singleunit
-* Return: Always 0 (success) 
+* Return: Always 0 (success)
 */
 
 int main(void)
 {
 int num;
-for(num = 48; num < 58; num++)
+for (num = 48; num < 58; num++)
 {
 putchar(num);
-if(num != 57)
+if (num != 57)
 {
 putchar(',');
 putchar(' ');
